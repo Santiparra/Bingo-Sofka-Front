4a3e093a05542ca6076a0247833326945e42235e
santiparra <122759810+Santiparra@users.noreply.github.com>
2024-11-07 17:57:58 -0300

    generated boilerplate files for planned components

---
+++ src/app/home/gameboard/scoresheet/scoresheet.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scoresheet',
+  standalone: true,
+  imports: [],
+  templateUrl: './scoresheet.component.html',
+  styleUrl: './scoresheet.component.css'
+})
+export class ScoresheetComponent {
+
+}
